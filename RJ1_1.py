44efa0ee5f54c52c5527047ec6a577a60e6ce8b4
Yuya-Ikegami <fdc65858@kwansei.ac.jp>
2022-11-09 12:40:38 +0900

    edit

--- RJ1_1.py
+++ RJ1_1.py
@@ -4,9 +4,9 @@ root = tk.Tk()
 root.geometry("500x400")
 
 # Canvasの作成
-canvas = tk.Canvas(root, bg = "white")
+canvas = tk.Canvas(root, bg="white")
 # Canvasを配置
-canvas.pack(fill = tk.BOTH, expand = True)
+canvas.pack(fill = tk.BOTH, expand=True)
 
 # 長方形の描画
 canvas.create_rectangle(100, 80, 400, 320, fill = "Black", width = 5)
